48d6b4c26f2d01587764a5f62102890821469d56
shahzain <shahzaintariq.crpyto@gmail.com>
2019-11-17 18:09:26 +0500

    make better

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-var rollNum = prompt('Enter Your Roll Nmber')
+var result = document.getElementById('btn');
+var rollNum = document.getElementById('search2');
 
-var xhttp = new XMLHttpRequest();
+const xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var response = JSON.parse(xhttp.responseText);
         const data = response.data
-        for(i=0; i<data.length; i++){
-            if(rollNum === data[i].RollNumber){
-                alert(`your Name is ${data[i].Name} and Your Percentile is ${data[i].Percentile}%`);
-                break
+        result.addEventListener('click', function zain(){
+            for(i=0;i<data.length;i++){
+            if(rollNum.value === data[i].RollNumber){
+                alert(`Name: ${data[i].Name} \nRoll Number:${data[i].RollNumber} \nRank ${data[i].Rank} \nPercentge ${data[i].Percentile}%`)
             }
-        }
+            }
+        });
+        
     }
 };
 xhttp.open("GET", "piaic.json", true);
